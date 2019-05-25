cfc99658aa161dd47395ca390e6e3c08fe2053c5
SimonB87 <buryans@gmail.com>
2019-05-25 18:24:46 +0200

    styles of OOP

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Javascript-OOP-Demo
+
+ - This is a demo from the [video tutorial](https://www.youtube.com/watch?v=PFmuCDHHpwk) by [Programming with Mosh](https://www.youtube.com/channel/UCWv7vMbMWH4-V0ZXdmDpPBA)
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('Start Code');
 
 // basic object function
-
+/*
 const circle = {
   radius: 1,
   location: { x: 1, y: 1 },
@@ -10,27 +10,27 @@ const circle = {
   }
 };
 circle.draw();
-
+*/
 // factorz function
 function createCircle(radius) {
   return {
     radius,
     draw: function() {
-      console.log('Draw');
+      document.getElementById("demo1").innerHTML = "Draw 1";
     }
   };
 }
 
-const circle = createCircle(1);
+const circle1 = createCircle(1);
+circle1.draw();
 
 // constructor function
 function Circle(radius) {
   this.radius = radius,
   this.draw = function() {
-    console.log('Draw');
+    document.getElementById("demo2").innerHTML = "Draw 2";
   }
 }
 
-const another = new Circle(1);
-
-//23-30
+const circle2 = new Circle(1);
+circle2.draw();
